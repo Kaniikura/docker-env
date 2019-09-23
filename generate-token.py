63bf51c6b03f2a6a956ef2f7f72842d46fcd7508
Takuya Ono <32826608+Kaniikura@users.noreply.github.com>
2019-09-24 01:31:44 +0900

    Update generate-token.py

--- generate-token.py
+++ generate-token.py
@@ -1,16 +1,13 @@
 import IPython as IPython
+from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    print("Generate a access token")
-    from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument("-p",
         "--password",
         dest="password",
-        help="The password you want to use for authentication.",
         required=True)
     args = parser.parse_args()
 
-    print("\nCopy this line into the .env file:\n")
     hash = IPython.lib.passwd(args.password)
-    print("ACCESS_TOKEN=" + hash)
+    print("アクセストークン -> " + hash)
